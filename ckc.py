3b6345e2e9d1f4741c3ba60c40da3eb3072252d6
ckc-rkeyel <130933438+ckc-rkeyel@users.noreply.github.com>
2023-04-18 19:28:55 -0400

    Update ckc.py
    Update to Python 3

--- ckc.py
+++ ckc.py
@@ -1,11 +1,10 @@
 # encoding=utf8
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+import importlib
+importlib.reload(sys)
 
 from dpla_api import DplaApi
 da = DplaApi()
 da.search("crochet* OR knit*", page_size=500)
 da.build_arc_rdf_dataset()
 da.create_tsv(output_path="data/ckc.txt")
-
